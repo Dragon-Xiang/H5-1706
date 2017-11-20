2794e7b0ea518019c8fdc8b2ea476b5e2f29bff0
Dragon-Xiang <506840830@qq.com>
2017-11-20 20:45:49 +0800

    交作业的

---
+++ user.sql
@@ -0,0 +1,5 @@
+INSERT INTO `user` (`id`, `username`, `password`) VALUES (1, '506840830@qq.com', '66666666');
+INSERT INTO `user` (`id`, `username`, `password`) VALUES (2, '15622144678', '33333333');
+INSERT INTO `user` (`id`, `username`, `password`) VALUES (3, '13112701403', '11111111');
+INSERT INTO `user` (`id`, `username`, `password`) VALUES (4, '13534575206', '99999999');
+INSERT INTO `user` (`id`, `username`, `password`) VALUES (5, '123456789@qq.com', 'aaaaaaaaa');
